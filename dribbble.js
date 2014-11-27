da93569893e7492c154132d32c36dd5551de40ff
Diego Costa <diego@diegocosta.com.br>
2014-11-26 23:13:03 -0300

    first commit

---
+++ dribbble.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	var divs = ".box-account-forgot-form, .box-account-forgot-link span.close";
+
+	$(divs).hide();
+
+	$('.box-account-forgot-link').click(function(){
+		$(divs).show();
+		return false;
+	});
+
+	$('.box-account-forgot-link span.close').click(function(){
+		$(divs).hide();
+		return false;
+	});
+
+	$('.box-bottom-reset').click(function(){
+		$(divs + ", .box-account-forgot-link").hide();
+		$(".box-account-forgot-success").show();
+		return false;
+	});
+
+});
