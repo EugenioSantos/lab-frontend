59391682c7747432b5a545f38f20edc4fd895a4b
Diego Costa <diego@diegocosta.com.br>
2014-11-27 06:55:40 -0300

    Redução do nome das classes

--- dribbble.js
+++ dribbble.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 
-	var divs = ".box-account-forgot-form, .box-account-forgot-link span.close";
+	var divs = ".box-forgot-form, .box-forgot-link span.close";
 
 	$(divs).hide();
 
-	$('.box-account-forgot-link').click(function(){
+	$('.box-forgot-link').click(function(){
 		$(divs).show();
 		return false;
 	});
 
-	$('.box-account-forgot-link span.close').click(function(){
+	$('.box-forgot-link span.close').click(function(){
 		$(divs).hide();
 		return false;
 	});
 
 	$('.box-bottom-reset').click(function(){
-		$(divs + ", .box-account-forgot-link").hide();
-		$(".box-account-forgot-success").show();
+		$(divs + ", .box-forgot-link").hide();
+		$(".box-forgot-success").show();
 		return false;
 	});
 
-	$('.box-account-button').click(function(){
+	$('.box-button').click(function(){
 		return false;
 	});
 
